9aebce544eadb3f5a1a6344eee661f62fde6ada1
nathan hishon <nathan.hishon@gmail.com>
2020-12-31 10:05:44 -0600

    tabs

--- js/tabs.js
+++ js/tabs.js
@@ -1,27 +1,33 @@
-document.querySelectorAll("#nav li").forEach(function(navEl) {
-  navEl.onclick = function() { toggleTab(this.id, this.dataset.target); }
+document.querySelectorAll("#nav li").forEach(function (navEl) {
+    navEl.onclick = function () {
+        toggleTab(this.id, this.dataset.target);
+    }
 });
 
+function focusPanel(id, target) {
+    toggleTab(id, target)
+}
+
 function toggleTab(selectedNav, targetId) {
-  var navEls = document.querySelectorAll("#nav li");
+    var navEls = document.querySelectorAll("#nav li");
 
-  navEls.forEach(function(navEl) {
-    if (navEl.id == selectedNav) {
-      navEl.classList.add("is-active");
-    } else {
-      if (navEl.classList.contains("is-active")) {
-        navEl.classList.remove("is-active");
-      }
-    }
-  });
+    navEls.forEach(function (navEl) {
+        if (navEl.id == selectedNav) {
+            navEl.classList.add("is-active");
+        } else {
+            if (navEl.classList.contains("is-active")) {
+                navEl.classList.remove("is-active");
+            }
+        }
+    });
 
-  var tabs = document.querySelectorAll(".tab-pane");
+    var tabs = document.querySelectorAll(".tab-pane");
 
-  tabs.forEach(function(tab) {
-    if (tab.id == targetId) {
-      tab.style.display = "block";
-    } else {
-      tab.style.display = "none";
-    }
-  });
+    tabs.forEach(function (tab) {
+        if (tab.id == targetId) {
+            tab.style.display = "block";
+        } else {
+            tab.style.display = "none";
+        }
+    });
 }
